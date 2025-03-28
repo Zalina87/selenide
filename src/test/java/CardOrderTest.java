cbdde781403699dc6185a34b1b542ec9e46b9798
Zalina <salina87@yandex.ru>
2025-03-28 22:28:33 +0300

    New Test

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.UUID;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -19,11 +20,11 @@ public class CardOrderTest {
 
     @BeforeAll
     static void setupAll() {
-        
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
+        //options.addArguments("--headless");
         Configuration.browserCapabilities = options;
     }
 
@@ -45,5 +46,29 @@ public class CardOrderTest {
 
         $("[data-test-id=notification]")
                 .shouldHave(Condition.text("Встреча успешно забронирована на " + date), Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldSubmitFormUsingAutoCompleteAndCalendar() {
+        $("[data-test-id=city] input").setValue("Ка");
+        $$(".menu-item").find(Condition.text("Казань")).click();
+        LocalDate targetDate = LocalDate.now().plusDays(7);
+        String targetDateString = targetDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        String targetDay = String.valueOf(targetDate.getDayOfMonth());
+        String targetMonthYear = targetDate.format(DateTimeFormatter.ofPattern("LLLL yyyy", new Locale("ru")));
+        $("[data-test-id=date] .calendar-input__custom-control").click();
+        $(".calendar").shouldBe(Condition.visible);
+        while(!$(".calendar__name").getText().equalsIgnoreCase(targetMonthYear)) {
+            $("[data-step='1'].calendar__arrow_direction_right").click();
+        }
+        $$("td.calendar__day").find(Condition.text(targetDay)).click();
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79636045632");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(Condition.text("Забронировать")).click();
+
+        $("[data-test-id=notification]")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + targetDateString), Duration.ofSeconds(15));
+
     }
 }
